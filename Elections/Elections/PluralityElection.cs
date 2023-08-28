6c90f143fe3711ef89e78874b69c34c83629f603
blinndcma <brian.t.linn@gmail.com>
2023-08-28 12:23:45 -0500

    interview exercise

--- Elections/Elections/PluralityElection.cs
+++ Elections/Elections/PluralityElection.cs
@@ -6,6 +6,48 @@ public class PluralityElection : IElection<ISingleVoteBallot>
 {
     public ICandidate Run(IReadOnlyList<ISingleVoteBallot> ballots, IReadOnlyList<ICandidate> candidates)
     {
-        throw new NotImplementedException();
+        if (ballots == null || !ballots.Any())
+            throw new ArgumentNullException(nameof(ballots));
+
+        if (candidates == null || !candidates.Any())
+            throw new ArgumentNullException(nameof(candidates));
+
+        var candidateVoteCount = new Dictionary<ICandidate, int>();
+
+        foreach (var ballot in ballots)
+        {
+            var candidateVote = ballot.Vote.Candidate;
+
+            //invalid candidate, skip
+            if (!candidates.Contains(candidateVote))
+                continue;
+
+            if (candidateVoteCount.ContainsKey(candidateVote))
+            {
+                candidateVoteCount[candidateVote]++;
+            }
+            else
+            {
+                candidateVoteCount.Add(candidateVote, 1);
+            }
+        }
+
+        var maxCount = candidateVoteCount.Values.Max();
+        var winningCandidate = candidateVoteCount.Where(c => c.Value == maxCount).ToList();
+
+        if (winningCandidate.Count == 1)
+        {
+            return winningCandidate.First().Key;
+        }
+        else if (winningCandidate.Count > 1)
+        {
+            throw new Exception("There is a tie.");
+        }
+        else if (winningCandidate.Count <= 0)
+        {
+            throw new Exception("Something bad happened.");
+        }
+
+        return null;
     }
 }
--- Elections/Elections/RankedChoiceElection.cs
+++ Elections/Elections/RankedChoiceElection.cs
@@ -6,6 +6,72 @@ public class RankedChoiceElection : IElection<IRankedBallot>
 {
     public ICandidate Run(IReadOnlyList<IRankedBallot> ballots, IReadOnlyList<ICandidate> candidates)
     {
-        throw new NotImplementedException();
+        if (ballots == null || !ballots.Any())
+            throw new ArgumentNullException(nameof(ballots));
+
+        var eliminatedCandidates = new List<ICandidate>();
+        
+        for (var i = 0; i < candidates.Count - 1; i++)
+        {
+            var candidateVoteCount = new Dictionary<ICandidate, int>();
+
+            foreach (var ballot in ballots)
+            {
+                foreach (var rankedVote in ballot.Votes.OrderBy(v => v.Rank))
+                {
+                    var candidateVote = rankedVote.Candidate;
+
+                    //invalid candidate, skip
+                    if (!candidates.Contains(candidateVote))
+                        continue;
+
+                    if (!eliminatedCandidates.Contains(candidateVote))
+                    {
+                        if (candidateVoteCount.ContainsKey(candidateVote))
+                        {
+                            candidateVoteCount[candidateVote]++;
+                        }
+                        else
+                        {
+                            candidateVoteCount.Add(candidateVote, 1);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            var maxCount = candidateVoteCount.Values.Max();
+            var totalValidBallots = candidateVoteCount.Values.Sum();
+            var percentage = (double) maxCount / totalValidBallots;
+
+            //if not majority winner, eliminate lowest candidate(s)
+            if (percentage < 0.5)
+            {
+                var lowestCount = candidateVoteCount.Values.Min();
+                var lowestCandidate = candidateVoteCount.Where(c => c.Value == lowestCount).ToList();
+
+                eliminatedCandidates.AddRange(lowestCandidate.Select(c => c.Key));
+            }
+            else
+            {
+                var winningCandidate = candidateVoteCount.Where(c => c.Value == maxCount).ToList(); 
+
+                if (winningCandidate.Count == 1)
+                {
+                    return winningCandidate.First().Key;
+                }
+                else if (winningCandidate.Count > 1)
+                {
+                    throw new Exception("There is a tie.");
+                }
+                else if (winningCandidate.Count <= 0)
+                {
+                    throw new Exception("Something bad happened.");
+                }
+            }
+        }
+
+        return null;
     }
 }
